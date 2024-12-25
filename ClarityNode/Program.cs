57f3b4cdbc01e23e6063b804d3ba564805123a4d
bas <baswelsink@hotmail.com>
2024-12-25 14:23:19 +0100

    Add project files.

---
+++ ClarityNode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClarityNode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
